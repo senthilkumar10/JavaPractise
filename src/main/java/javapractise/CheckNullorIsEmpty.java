3ff4b361cf88b87bc797abc0ea2966c7b6d5cbe7
Senthil Kumar <senthilkumar10@gmail.com>
2019-06-20 21:28:48 +0200

    Check Null using StringUtils from Commons Lang3

--- src/main/java/javapractise/CheckNullorIsEmpty.java
+++ src/main/java/javapractise/CheckNullorIsEmpty.java
@@ -1,5 +1,7 @@
 package javapractise;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class CheckNullorIsEmpty {
 
 	public static void main(String[] args) {
@@ -7,16 +9,27 @@ public class CheckNullorIsEmpty {
 		String input = null;
 		String input1 = "";
 		String input2 = "test";
+		String input3 = " ";
 		
 		System.out.println(!isNullorEmpty(input));
 		System.out.println(!isNullorEmpty(input1));
 		System.out.println(!isNullorEmpty(input2));
-
+		System.out.println(!isNullorEmpty(input3));
+		
+		System.out.println(!StringUtils.isBlank(input));
+		System.out.println(!StringUtils.isBlank(input1));
+		System.out.println(!StringUtils.isBlank(input2));
+		System.out.println(!StringUtils.isBlank(input3));
+		
+		
+		
 	}
 	
 	
 	public static boolean isNullorEmpty(String input) {
-		return (input==null || input.isEmpty())? true: false;
+		return (input==null || input.isEmpty());
 	}
+	
+	
 
 }
---
+++ src/main/java/javapractise/OsName.java
@@ -0,0 +1,17 @@
+package javapractise;
+
+
+
+public class OsName {
+
+	public static void main(String[] args) {
+		
+		String osName = System.getProperty("os.name");
+		
+		System.out.println(osName);
+
+		
+		
+	}
+
+}
